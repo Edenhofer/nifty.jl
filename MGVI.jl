cf2da801d7be90a712cc169b51f2f9fb6b1046e4
Gordian Edenhofer <gordian.edenhofer@gmail.com>
2020-06-16 00:37:24 +0200

    Outsource to concepts to functions and structs
    Additionally, use multiple dispatch excessively as to help Julia figure
    out which calculations it can optimize.

--- MGVI.jl
+++ MGVI.jl
@@ -10,6 +10,100 @@ using LinearMaps
 using Statistics: mean
 using Plots
 
+using Optim
+
+struct NegLogLikelihoodWithMetric{T<:Function,V<:Function}
+	nll::T
+	metric::AbstractMatrix
+	jac_at::V
+end
+
+struct StandardHamiltonian{T<:Function}
+	nll_plus_prior::T
+	nll_with_metric::NegLogLikelihoodWithMetric
+end
+
+mutable struct Energy{T<:Function}
+	potential::T
+	position::Vector{Float64}
+	samples::Union{Array{Vector{Float64}},Nothing}
+	curvature::Union{LinearMap,AbstractMatrix,Nothing}
+end
+
+function jacobian(f::T where T<:Function, ξ::V) where V
+	to_dual(δ::V) = map((v, p) -> ForwardDiff.Dual(v, p...), ξ, δ)
+	jvp(δ::V) = mapreduce(ForwardDiff.partials, vcat, f(to_dual(δ)))
+
+	vjp(δ::V) = first(Zygote.pullback(f, ξ)[2](δ))
+
+	return LinearMap{eltype(ξ)}(jvp, vjp, first(size(ξ)))
+end
+
+function covariance_sample(
+	cov_inv::T where T<:LinearMap,
+	jac::LinearMap{V},
+	metric::Union{LinearMap,AbstractMatrix}
+) where V
+	ξ_new::Vector{V} = randn(first(size(cov_inv)))
+	d_new::Vector{V} = jac * ξ_new .+ sqrt(inv(metric)) * randn(dims)
+	j_new::Vector{V} = adjoint(jac) * metric * d_new
+	m_new::Vector{V} = cg(cov_inv, j_new, log=true)[1]
+    return ξ_new .- m_new
+end
+
+function gaussian_energy(
+	noise_cov::AbstractMatrix,
+	data::Vector{T},
+	signal_response::V where V<:Function
+) where T<:Number
+	inv_noise_cov = inv(noise_cov)
+	function nll(ξ::Vector{T})
+		res = data .- signal_response(ξ)
+		return transpose(res) * inv_noise_cov * res
+	end
+	jac_at(ξ::Vector{T}) = jacobian(signal_response, ξ)
+
+	return NegLogLikelihoodWithMetric(nll, inv_noise_cov, jac_at)
+end
+
+function standard_hamiltonian(nll_w_metric::NegLogLikelihoodWithMetric)
+	nll_plus_pr(ξ::T where T) = nll_w_metric.nll(ξ) + 0.5 * (ξ ⋅ ξ)
+	return StandardHamiltonian(nll_plus_pr, nll_w_metric)
+end
+
+function metric_gaussian_kl(
+	standard_ham::StandardHamiltonian,
+	pos::T,
+	n_samples::V where V<:Int;
+	mirror_samples::Bool=false
+) where T
+	jac = standard_ham.nll_with_metric.jac_at(pos)
+	metric = standard_ham.nll_with_metric.metric
+	nll_fisher(ξ::T) = adjoint(jac) * metric * jac * ξ
+	fisher = LinearMap(nll_fisher, size(metric)[2]) + I
+
+	samples = [covariance_sample(fisher, jac, metric) for i = 1 : n_samples]
+
+	ham = standard_ham.nll_plus_prior
+	# TODO: convert samples to an iteration that can be mirrored
+	samples = mirror_samples ? vcat(samples, -samples) : samples
+	kl(ξ::T) = reduce(+, ham(ξ + s) for s in samples) / length(samples)
+
+	return Energy(kl, pos, samples, fisher)
+end
+
+function max_posterior(standard_ham::StandardHamiltonian, pos)
+	return Energy(standard_ham.nll_plus_prior, pos, nothing, nothing)
+end
+
+function minimize!(energy::Energy{T}; nat_grad_scl=1.) where T
+	# TODO: take metric of KL itself, i.e. the averaged one
+	Δξ = cg(energy.curvature, first(gradient(energy.potential, energy.position)), log=true)[1]
+	energy.position .-= nat_grad_scl * Δξ
+	return energy
+end
+
+
 dims = (1024)
 
 # ξ := latent variables
@@ -32,7 +126,7 @@ function *(trafo::typeof(ht), u::Vector{ForwardDiff.Dual{T,V,P}}) where {T,V,P}
 	# Pack SoA -> AoS (depending on jvp, might need `eachrow`)
 	return map((v, p) -> ForwardDiff.Dual{T}(v, p...), val, jvp)
 end
-Zygote.@adjoint function *(trafo::typeof(inv(ht)), xs)
+Zygote.@adjoint function *(trafo::typeof(inv(ht)), xs::T) where T
 	return trafo * xs, Δ -> (nothing, trafo * Δ)
 end
 Zygote.@adjoint function inv(trafo::typeof(ht))
@@ -60,71 +154,23 @@ plot(ss, color=:red, label="ground truth", linewidt=5)
 
 signal_response(ξ) = R * signal(ξ)
 # Negative log-likelihood assuming a Gaussian energy
-nll(ξ) = sum(N^-1 * (d .- signal_response(ξ)).^2)
-# Standard-Hamiltonian
-ham(ξ) = nll(ξ) + sum(ξ.^2)
-# Metric of the given energy (here a Gaussian one)
-M = N^-1
-
-function jacobian(f, ξ)
-	to_dual(δ) = map((v, p) -> ForwardDiff.Dual(v, p...), ξ, δ)
-	jvp(δ) = mapreduce(ForwardDiff.partials, vcat, f(to_dual(δ)))
-
-	vjp(δ) = first(Zygote.pullback(f, ξ)[2](δ))
-
-	return LinearMap{eltype(ξ)}(jvp, vjp, first(size(ξ)))
-end
-
-function local_fisher(jac)
-	nll_fish(ξ) = adjoint(jac) * M * jac * ξ
-	return LinearMap(nll_fish, dims) + I
-end
-
-function implicit_covariance_sample(cov_inv, jac)
-    ξ_new = randn(dims)
-	d_new = jac * ξ_new .+ R * sqrt(M^-1) * randn(dims)
-	j_new = adjoint(jac) * M * d_new
-	m_new = cg(cov_inv, j_new, log=true)[1]
-    return ξ_new - m_new
-end
-
-function mgvi!(pos, n_samples; nat_grad_scl=1., mirror_samples=false)
-	jac = jacobian(signal_response, pos)
-	fish = local_fisher(jac)
-	println("sampling...")
-	smpls = [implicit_covariance_sample(fish, jac) for i = 1 : n_samples]
-
-	println("minimizing...")
-	if mirror_samples
-		kl(ξ) = reduce(+, ham(ξ + s) + ham(ξ - s) for s in smpls) / (2 * n_samples)
-		n_eff_smpls = 2 * n_samples
-	else
-		kl(ξ) = reduce(+, ham(ξ + s) for s in smpls) / n_samples
-		n_eff_smpls = n_samples
-	end
-
-	# TODO: take metric of KL itself, i.e. the averaged one
-	Δξ = cg(fish, first(gradient(kl, pos)), log=true)[1]
-	println("computing natural gradient...")
-	pos .-= nat_grad_scl * Δξ
-	return pos, smpls
-end
-
-init_pos = 0.1 * randn(dims)
+ge = gaussian_energy(N, d, signal_response)
+ham = standard_hamiltonian(ge)
 
 n_samples = 3
+init_pos = 0.1 * randn(dims)
 pos = copy(init_pos)
-# Warm up in a region where the gradient is not yet very meaningful
-samples = mgvi!(pos, 1; nat_grad_scl=1e-2, mirror_samples=true)[2]
-samples = mgvi!(pos, 1; nat_grad_scl=1e-1, mirror_samples=true)[2]
-for i in 1:10
-	global pos, samples
-	samples = mgvi!(pos, n_samples; nat_grad_scl=1.; mirror_samples=true)[2]
-	#plot!(signal(pos), label="it. " * string(i))
+mgkl = metric_gaussian_kl(ham, pos, n_samples; mirror_samples=true)
+minimize!(mgkl; nat_grad_scl=1e-2)
+for i in 1 : 3
+	global pos, mgkl
+	pos .= mgkl.position
+	mgkl = metric_gaussian_kl(ham, pos, n_samples; mirror_samples=true)
+	minimize!(mgkl; nat_grad_scl=1.)
 end
 
-for (i, s) in enumerate(samples)
+for (i, s) in enumerate(mgkl.samples)
 	plot!(signal(pos + s), label="Post. Sample " * string(i), color=:gray)
 end
-plot!(signal(pos), label="Post. Mean", color=:orange)
+plot!(signal(mgkl.position), label="Post. Mean", color=:orange)
 savefig("mgvi_example.pdf")
