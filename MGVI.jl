46ef4ae3cae42177085a9a1bd2a266d1d7f19424
Gordian Edenhofer <gordian.edenhofer@gmail.com>
2020-06-17 14:44:04 +0200

    MGVI.jl: Excessive multiple dispatch

--- MGVI.jl
+++ MGVI.jl
@@ -12,25 +12,25 @@ using Plots
 
 using Optim
 
-struct NegLogLikelihoodWithMetric{T<:Function,V<:Function}
-	nll::T
-	metric::AbstractMatrix
-	jac_at::V
+struct NegLogLikelihoodWithMetric{T<:Union{AbstractMatrix,LinearMap}}
+	nll::F where F<:Function
+	metric::T
+	jac_at::F where F<:Function
 end
 
-struct StandardHamiltonian{T<:Function}
-	nll_plus_prior::T
-	nll_with_metric::NegLogLikelihoodWithMetric
+struct StandardHamiltonian{T}
+	nll_plus_prior::F where F<:Function
+	nll_with_metric::NegLogLikelihoodWithMetric{T}
 end
 
-mutable struct Energy{T<:Function}
-	potential::T
-	position::Vector{Float64}
-	samples::Union{Array{Vector{Float64}},Nothing}
-	curvature::Union{LinearMap,AbstractMatrix,Nothing}
+mutable struct Energy{T, L<:Union{Nothing,Array{T}}, N<:Union{Nothing,AbstractMatrix,LinearMap}}
+	potential::F where F<:Function
+	position::T
+	samples::L
+	curvature::N
 end
 
-function jacobian(f::T where T<:Function, ξ::V) where V<:Union{<:Number,Vector{<:Number}}
+function jacobian(f::F, ξ::V) where {F<:Function, V<:Union{Number,Vector{<:Number}}}
 	to_dual(δ::V) = map((v, p) -> ForwardDiff.Dual(v, p...), ξ, δ)
 	jvp(δ::V) = mapreduce(ForwardDiff.partials, vcat, f(to_dual(δ)))
 
@@ -39,63 +39,61 @@ function jacobian(f::T where T<:Function, ξ::V) where V<:Union{<:Number,Vector{
 	return LinearMap{eltype(ξ)}(jvp, vjp, first(size(ξ)))
 end
 
-function covariance_sample(
-	cov_inv::T where T<:LinearMap,
-	jac::LinearMap{V},
-	metric::Union{LinearMap,AbstractMatrix}
-) where V
-	ξ_new::Vector{V} = randn(first(size(cov_inv)))
-	d_new::Vector{V} = jac * ξ_new .+ sqrt(inv(metric)) * randn(dims)
-	j_new::Vector{V} = adjoint(jac) * metric * d_new
-	m_new::Vector{V} = cg(cov_inv, j_new, log=true)[1]
-    return ξ_new .- m_new
+function covariance_sample(cov_inv::T, jac::N, metric::M) where {
+	T<:Union{AbstractMatrix,LinearMap{E}},
+	N<:Union{AbstractMatrix,LinearMap{E}},
+	M<:Union{AbstractMatrix,LinearMap{E}}
+} where E
+	ξ_new::Vector{E} = randn(first(size(cov_inv)))
+	d_new::Vector{E} = jac * ξ_new .+ sqrt(inv(metric)) * randn(dims)
+	j_new::Vector{E} = adjoint(jac) * metric * d_new
+	m_new::Vector{E} = cg(cov_inv, j_new, log=true)[1]
+	return ξ_new .- m_new
 end
 
-function gaussian_energy(
-	noise_cov::AbstractMatrix,
-	data::Vector{T},
-	signal_response::V where V<:Function
-) where T<:Number
+function gaussian_energy(noise_cov::T, data::V, signal_response::F) where {
+	T<:Union{AbstractMatrix,LinearMap{E}},
+	V<:AbstractVector{E},
+	F<:Function
+} where E
 	inv_noise_cov = inv(noise_cov)
-	function nll(ξ::Vector{T})
+	function nll(ξ::V)
 		res = data .- signal_response(ξ)
 		return transpose(res) * inv_noise_cov * res
 	end
-	jac_at(ξ::Vector{T}) = jacobian(signal_response, ξ)
+	jac_at(ξ::V) = jacobian(signal_response, ξ)
 
 	return NegLogLikelihoodWithMetric(nll, inv_noise_cov, jac_at)
 end
 
-function standard_hamiltonian(nll_w_metric::NegLogLikelihoodWithMetric)
+function standard_hamiltonian(nll_w_metric::NegLogLikelihoodWithMetric{T}) where T
 	nll_plus_pr(ξ::T where T) = nll_w_metric.nll(ξ) + 0.5 * (ξ ⋅ ξ)
 	return StandardHamiltonian(nll_plus_pr, nll_w_metric)
 end
 
 function metric_gaussian_kl(
-	standard_ham::StandardHamiltonian,
-	pos::T,
-	n_samples::V where V<:Int;
+	standard_ham::StandardHamiltonian{T},
+	pos::P,
+	n_samples::C;
 	mirror_samples::Bool=false
-) where T
+) where {T, P, C<:Int}
 	jac = standard_ham.nll_with_metric.jac_at(pos)
 	metric = standard_ham.nll_with_metric.metric
-	nll_fisher(ξ::T) = adjoint(jac) * metric * jac * ξ
-	fisher = LinearMap(nll_fisher, size(metric)[2]) + I
+	fisher = adjoint(jac) * metric * jac + I
 
 	samples = [covariance_sample(fisher, jac, metric) for i = 1 : n_samples]
 
 	ham = standard_ham.nll_plus_prior
 	# TODO: convert samples to an iteration that can be mirrored
 	samples = mirror_samples ? vcat(samples, -samples) : samples
-	kl(ξ::T) = reduce(+, ham(ξ + s) for s in samples) / length(samples)
+	kl(ξ::P) = reduce(+, ham(ξ + s) for s in samples) / length(samples)
 
 	# Take the metric of the KL itself as curvature
-	nll_fisher_by_s = []
-	for s in samples
+	nll_fisher_by_s = mapreduce(+, samples) do s
 		jac_s = standard_ham.nll_with_metric.jac_at(pos + s)
-		nll_fisher_by_s = vcat(nll_fisher_by_s, adjoint(jac_s) * metric * jac_s)
+		return adjoint(jac_s) * metric * jac_s
 	end
-	avg_fisher = (reduce(+, nll_fisher_by_s)) / length(samples) + I
+	avg_fisher = nll_fisher_by_s / length(samples) + I
 
 	return Energy(kl, pos, samples, avg_fisher)
 end
@@ -104,7 +102,7 @@ function max_posterior(standard_ham::StandardHamiltonian, pos)
 	return Energy(standard_ham.nll_plus_prior, pos, nothing, nothing)
 end
 
-function minimize!(energy::Energy{T}; nat_grad_scl=1.) where T
+function minimize!(energy::Energy{T}; nat_grad_scl=1) where T
 	Δξ = cg(energy.curvature, first(gradient(energy.potential, energy.position)), log=true)[1]
 	energy.position .-= nat_grad_scl * Δξ
 	return energy
@@ -148,7 +146,7 @@ P = Diagonal(@. 50 / (k^2.5 + 1))
 draw_sample(ξ::T where T) = inv(ht) * (P * ξ)
 signal(ξ::T where T) = exp.(draw_sample(ξ))
 
-N = Diagonal(0.1^2 * ones(dims))
+N = Diagonal(0.01^2 * ones(dims))
 R = ones(dims)
 #R[100:200] .= 0
 R = Diagonal(R)
@@ -157,7 +155,7 @@ R = Diagonal(R)
 ss = signal(ξ_truth)
 d = R * ss .+ R * sqrt(N) * randn(dims)
 plot(ss, color=:red, label="ground truth", linewidt=5)
-#plot!(d, seriestype=:scatter, marker=:x, color=:black)
+plot!(d, seriestype=:scatter, marker=:x, color=:black)
 
 signal_response(ξ::T where T) = R * signal(ξ)
 # Negative log-likelihood assuming a Gaussian energy
